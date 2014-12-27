923e8f640ce5b3989b0b53443b7456c938189dc7
hjq <huhuh1234567@126.com>
2014-12-27 20:44:21 +0800

    add ajax demo

---
+++ ajax.js
@@ -0,0 +1,18 @@
+(function(){
+
+	window.WebRPC = window.WebRPC||{};
+
+	WebRPC.ajax = function(url,data,successHandler,errorHandler){
+		var xhr = new XMLHttpRequest();
+		xhr.onload = function(){
+			successHandler&&successHandler(JSON.parse(xhr.responseText));
+		};
+		xhr.onerror = function(){
+			errorHandler&&errorHandler(xhr.status,JSON.parse(xhr.responseText));
+		};
+		xhr.open("post",url,true);
+		xhr.setRequestHeader("Content-Type","text/plain");
+		xhr.send(JSON.stringify(data));
+	}
+
+})();
